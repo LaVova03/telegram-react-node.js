a7cce7ca99951bf384ad3d809b8cc5a6768874e2
Volodymyr <davidvovchik@gmail.com>
2024-06-06 15:39:01 +0300

    f

--- index.js
+++ index.js
@@ -104,9 +104,9 @@ app.post('/web-data', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
